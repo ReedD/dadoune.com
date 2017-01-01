b74dcf5bc8a4ae173bafdf39e6595e39af1fe72f
Reed Dadoune <reed@dadoune.com>
2016-12-31 18:47:21 -0800

    Utilize browserify `insertGlobalVars`

--- src/assets/app/directives/offline.js
+++ src/assets/app/directives/offline.js
@@ -14,7 +14,7 @@ angular
 		function ($window)  {
 			function getCache () {
 				return $window.caches
-					.open('v{{ PACKAGE.VERSION }}::pages')
+					.open(`v${__siteVersion}::pages`)
 					.then(cache => {
 						return cache.keys();
 					})
