2a70dc7b012899ad2754508024c8ef39e228d805
Reed Dadoune <reed@dadoune.com>
2018-08-24 21:00:48 -0700

    feat(pwa): Add manifest.json

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,21 @@ const Content = styled.div`
   min-height: 100vh;
 `;
 
+const title = 'Reed Dadoune | www.dadoune.com';
+const description = "Reed Dadoune's blog and project portfolio";
+
 const App = () => (
   <div>
     <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="theme-color" content="#ffffff" />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="/icon-512x512.png" />
+      <meta property="og:site_name" content="Reed Dadoune" />
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <link rel="manifest" href="manifest.json" />
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"
