6d874c1b55d5f29884c81d5bef3b1118474ff2da
Reed Dadoune <reed@dadoune.com>
2018-08-24 19:25:44 -0700

    feat (pwa): Add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Your top level component
 import App from './App';
 
-// Export your top level component as JSX (for static rendering)
 export default App;
 
-// Render your app
 if (typeof document !== 'undefined') {
   const renderMethod = module.hot
     ? ReactDOM.render
@@ -16,6 +12,11 @@ if (typeof document !== 'undefined') {
     renderMethod(<Comp />, document.getElementById('root'));
   };
 
-  // Render!
   render(App);
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js');
+    });
+  }
 }
